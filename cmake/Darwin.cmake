c4c1d6bc979149046fc703bc7876d386a3f04263
Łukasz Kostka <lukasz.kostka@netng.pl>
2018-11-17 00:12:47 +0100

    Remove Qt5_DIR env var

--- cmake/Darwin.cmake
+++ cmake/Darwin.cmake
@@ -10,3 +10,10 @@ set(MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}")
 set(MACOSX_BUNDLE_COPYRIGHT ${COPYRIGHT})
 set(MACOSX_BUNDLE_GUI_IDENTIFIER ${IDENTIFIER})
 set(MACOSX_BUNDLE_ICON_FILE ${ICON_NAME}.icns)
+
+
+set(CMAKE_PREFIX_PATH  "/usr/local/lib;/usr/local/opt/qt5;/opt/local/lib;${CMAKE_PREFIX_PATH}")
+set(CMAKE_INCLUDE_PATH "/usr/local/include;/usr/local/opt/qt5/include;/opt/local/include;${CMAKE_INCLUDE_PATH}")
+set(CMAKE_LIBRARY_PATH "/usr/local/lib;/usr/local/opt/qt5/lib;/opt/local/lib;${CMAKE_LIBRARY_PATH}")
+set(CMAKE_PROGRAM_PATH "/usr/local/bin;/usr/local/opt/qt5/bin;/opt/local/bin;${CMAKE_PROGRAM_PATH}")
+include_directories("/usr/local/include" "/usr/local/opt/qt5/include" "/opt/local/include")
